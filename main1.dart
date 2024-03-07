acf5f08fdd540c4e535320b21575cbf8f3293e30
Daniel Nyongesa <wekesadaniel@students.uonbi.ac.ke>
2024-03-07 15:59:12 +0300

    Looping code

---
+++ main1.dart
@@ -0,0 +1,24 @@
+void main() {
+  var a = 1;
+  var maxnum = 11;
+  while (a < maxnum) {
+    print(a);
+    a = a + 1;
+  }
+  var age = 16;
+  if (age > 18) {
+    print("Daniel is a voter in kenya");
+  } else {
+    print("Daniel is still young to vote");
+  }
+  int num = 1;
+  for (num; num <= 3; num++) {
+    print(num);
+  }
+  {
+    var list1 = [10, 20, 30, 40, 50];
+    for (var i in list1) {
+      print(i);
+    }
+  }
+}
